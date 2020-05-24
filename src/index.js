0a81f9d912e01ad18d120db76dd098b94f350b29
Sean Haneberg <haneberg@gmail.com>
2020-05-23 23:06:22 -0700

    feat: gamepad support - camera & cursor movement
    Squashed commit of the following:
    
    commit 02ee2afe5f7e68d71eb66c4f2359232cbe40c5d2
    Author: Sean Haneberg <haneberg@gmail.com>
    Date:   Sat May 23 23:06:04 2020 -0700
    
        initial controller support
    
    commit 23359f697def53679e4a7cd7d73f7a8319a14892
    Author: Sean Haneberg <haneberg@gmail.com>
    Date:   Fri May 22 22:04:59 2020 -0700
    
        basic button processing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@
 import DoctorGameEngine from './engine';
 import WebView from './view';
 
+let throttled = false;
+
 let moveCursor = (xDelta, yDelta) => {
+    if (throttled) {
+        return;
+    }
+
+    throttled = true;
+
     if (engine.moveCursor(xDelta, yDelta)) {
         let position = engine.getCursorPosition();
 
@@ -11,7 +19,23 @@ let moveCursor = (xDelta, yDelta) => {
         cursor.position.x = newPosition.x;
         cursor.position.y = newPosition.y;
     }
+
+    setTimeout(() => throttled = false, 250);
 };
+
+let gamepadInit = false;
+
+window.addEventListener('gamepadconnected', (event) => {
+    console.log(`gamepadconnected:`);
+    console.log(event);
+    gamepadInit = true;
+});
+
+window.addEventListener('gamepaddisconnected', (event) => {
+    console.log(`gamepaddisconnected:`);
+    console.log(event);
+    gamepadInit = false;
+});
 
 let cameraSpeed = 1;
 document.body.addEventListener('keydown', (event) => {
@@ -56,6 +80,81 @@ let options = {
     minMatchSize: 3
 };
 
+let processControllerInput = () => {
+    if (!gamepadInit) {
+        return;
+    }
+
+    let gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+
+    for (let padIndex = 0; padIndex < gamepads.length; padIndex++) {
+        let gamepad = gamepads[padIndex];
+
+        if (!gamepad) {
+            continue;
+        }
+
+        gamepad.axes.forEach((val, axesIndex) => {
+
+            let rotationSpeed = 0.05;
+            // Don't print neutral axes.
+            if (val >= 0.004 || val <= -0.004) {
+                // console.log(`axes ${axesIndex} with ${val}`);
+                switch (axesIndex) {
+                    case 0:
+                        camera.position.x += cameraSpeed * val;
+                        break;
+                    case 1:
+                        camera.position.z += cameraSpeed * val;
+                        break;
+                    case 2:
+                        camera.rotation.y -= rotationSpeed * val;
+                        break;
+                    case 3:
+                        camera.rotation.x += rotationSpeed * val;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        gamepad.buttons.forEach((val, buttonIndex) => {
+            let pressed = (val == 1.0);
+            let value = val;
+
+            if (typeof(val) == "object") {
+                pressed = val.pressed;
+                value = val.value;
+            }
+
+            if (pressed) {
+                console.log(`button ${buttonIndex} pressed on ${padIndex} with ${value}`);
+
+                switch(buttonIndex) {
+                    case 12:
+                        moveCursor(0, -1);
+                        break;
+                    case 13:
+                        moveCursor(0, 1);
+                        break;
+                    case 14:
+                        moveCursor(-1, 0);
+                        break;
+                    case 15:
+                        moveCursor(1, 0);
+                        break;
+                }
+            }
+
+        });
+    }
+};
+
+let update = () => {
+    processControllerInput();
+};
+
 let engine = new DoctorGameEngine(options);
 let view = new WebView(engine);
 let renderer = view.getRenderer();
@@ -64,6 +163,8 @@ let camera = view.getCamera();
 
 var animate = function () {
     requestAnimationFrame( animate );
+
+    update();
     renderer.render( scene, camera );
 };
 
